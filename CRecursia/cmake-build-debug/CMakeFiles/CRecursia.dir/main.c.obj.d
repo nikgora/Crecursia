8ba904bcf7e7675a67aa6e5169025da9ff13732d
NIk <nik.gora.2004@gmail.com>
2022-06-22 12:52:59 +0200

    1  2 3 ready 4 not ready

---
+++ CRecursia/cmake-build-debug/CMakeFiles/CRecursia.dir/main.c.obj.d
@@ -0,0 +1,16 @@
+CMakeFiles/CRecursia.dir/main.c.obj: \
+ C:\Users\Admin\Documents\ALl\University\C\Crecursia\CRecursia\main.c \
+ C:/Users/Admin/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5787.29/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
+ C:/Users/Admin/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5787.29/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/Users/Admin/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5787.29/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ C:/Users/Admin/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5787.29/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Users/Admin/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5787.29/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Users/Admin/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5787.29/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Users/Admin/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5787.29/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Users/Admin/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5787.29/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Users/Admin/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5787.29/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/Users/Admin/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5787.29/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/Users/Admin/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5787.29/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/Users/Admin/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5787.29/bin/mingw/x86_64-w64-mingw32/include/string.h \
+ C:/Users/Admin/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5787.29/bin/mingw/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/Users/Admin/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5787.29/bin/mingw/x86_64-w64-mingw32/include/sec_api/string_s.h
--- CRecursia/main.c
+++ CRecursia/main.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
+#include <string.h>
+
+#define N 100
 
 int gcd(int a, int b);
 
 int makkarti(int n);
 
+void reversePrint(char string[N], int index);
+
+int countLetter(char string[N], char letters[N], int res, int index);
+
+void print(char string[N], int index);
+
 int main() {
-    printf("%d", makkarti(91));
+    char string[N] = "Asp";
+    char letters[N] = "Asp";
+    int a, b, c, d;
+    //scanf("%d%d%d%d", &a, &b, &c, &d);
+    //printf("gcd(a, b) = %d\ngcd(a, c) = %d\ngcd(a, d) = %d\n", gcd(a, b), gcd(a, c), gcd(a, d));
+    //printf("%d", makkarti(91));
+    //print(string, 0);
+    //reversePrint(string,0);
+    printf("%d", countLetter(string, letters, 0, 0));
     return 0;
+}
+
+int countLetter(char string[N], char letters[N], int res, int index) {
+    for (int i = 0; i < strlen(letters) && index < strlen(string); ++i) {
+        if (letters[i] == string[index])res++;
+    }
+    return index < strlen(string) ? countLetter(string, letters, res, index + 1) : res;
 }
 
 int gcd(int a, int b) {
@@ -15,4 +39,14 @@ int gcd(int a, int b) {
 
 int makkarti(int n) {
     return n > 100 ? n - 10 : makkarti(makkarti(n + 11));
+}
+
+void reversePrint(char string[N], int index) {
+    if (index < strlen(string)) reversePrint(string, index + 1);
+    printf("%c", string[index - 1]);
+}
+
+void print(char string[N], int index) {
+    printf("%c", string[index]);
+    if (index < strlen(string)) print(string, index + 1);
 }
