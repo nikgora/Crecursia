4337fc9adefa11818c5c21a5cdcfb2511de65b32
NIk <nik.gora.2004@gmail.com>
2022-06-22 12:24:15 +0200

    1  2 ready 3 4 not ready

---
+++ CRecursia/cmake-build-debug/CMakeFiles/CRecursia.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CRecursia.dir/main.c.obj"
+  "CMakeFiles/CRecursia.dir/main.c.obj.d"
+  "CRecursia.exe"
+  "CRecursia.exe.manifest"
+  "CRecursia.pdb"
+  "libCRecursia.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CRecursia.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
